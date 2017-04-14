4744474bcdaf5bce39de85ba0918c2532359b27a
IvanCheresky <ivancheresky@gmail.com>
2017-04-14 19:42:04 -0300

    ?

--- app/src/main/java/com/dernysoftware/bolichapp/HomeFragment.java
+++ app/src/main/java/com/dernysoftware/bolichapp/HomeFragment.java
@@ -6,7 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListView;
+
+import com.dernysoftware.bolichapp.Posts.Post;
+
+import java.util.ArrayList;
+
+import static com.dernysoftware.bolichapp.MainMenu.dbBoliches;
 
 /**
  * Created by Ivan on 13/04/2017.
@@ -15,6 +23,10 @@ import android.widget.ImageButton;
 public class HomeFragment extends Fragment implements View.OnClickListener {
 
     private View v;
+    private ListView viewBoliches;
+    public ArrayAdapter adapter;
+    public ArrayList<Post> posts;
+
 
     @Override
     public void onResume() {
@@ -30,7 +42,11 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         ImageButton ib = (ImageButton) v.findViewById(R.id.refreshButton);
         ib.setOnClickListener(this);
 
+        viewBoliches = (ListView) v.findViewById(R.id.viewBoliches);
 
+        if(posts != null){
+            showBoliches(posts);
+        }
 
         return v;
     }
@@ -43,6 +59,12 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
                 refresh();
                 break;
         }
+    }
+
+    public void showBoliches(ArrayList<Post> posts){
+        this.posts = posts;
+        adapter = new ArrayAdapter<Post>(getActivity(),android.R.layout.simple_list_item_multiple_choice, posts);
+        viewBoliches.setAdapter(adapter);
     }
 
     private void refresh(){
